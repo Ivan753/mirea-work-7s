8cec9459f50b97942dcf1d29ef35c1256dc940cf
Ivan Bugaev <ivan-753s@mail.ru>
2020-10-04 20:46:18 +0300

    Decode stairs code

---
+++ is/stairs_decoder.py
@@ -0,0 +1,49 @@
+"""
+Дешифрация текста, полученного применением шифра "Лесенка"
+к исходному тексту
+Аргументы: <начальное количество ступеней> <конечное количество ступеней> <текст>
+"""
+import sys
+
+
+if __name__ == '__main__':
+    args = sys.argv
+
+    if len(args) < 3:
+        raise Exception("Неверное количество аргументов")
+
+    text = list(
+      args[3]
+    )
+
+    start = int(args[1])
+    finish = int(args[2])
+
+
+    for i in range(start, finish):
+        splitedText = []
+        # получаем длину ступени без остатка
+        partsLength = len(text) // i
+        # смотрим, нужен ли остаток (добавляется по единице к строке, пока остаток не закончится)
+        add = 0
+        if (len(text)%i > 0):
+            add = 1
+
+        k = 0 # текущий начальный символ, идентифицирубщий новую ступень
+        for j in range(i):
+            # если новер ступени превышает остаток, то единица не добавляется
+            if k // partsLength >= len(text)%i:
+                add = 0
+
+            splitedText.append(text[k:k+partsLength+add])
+            k = k + partsLength + add
+
+        print()
+        print()
+        print(str(i)+".", end=" ")
+        for ii in range(len(splitedText[0])):
+            for jj in range(len(splitedText)):
+                if (ii >= len(splitedText[jj])):
+                    continue
+
+                print(splitedText[jj][ii], end=" ")
